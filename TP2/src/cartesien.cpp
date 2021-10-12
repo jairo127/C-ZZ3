76a724973e1af343455e2be4880dbfb3f2566a0f
William <william.garrier63@orange.fr>
2021-10-12 10:20:52 +0200

    Point, Cartesien, Polaire copy
    On suppose que les classes Point, Cartésien et
    Polaire ont été correctement définies et sont fonctionnelles, et que les moyens de conversion entre
    Cartésien et Polaire existent. Copiez le code source de ces classes dans le répertoire tp_2/src et
    modifiez CMakeLists.txt pour que CMake les prenne en compte.

---
+++ TP2/src/cartesien.cpp
@@ -0,0 +1,40 @@
+#include "cartesien.hpp"
+#include "polaire.hpp"
+
+Cartesien::Cartesien() : _x(0), _y(0) {}
+
+Cartesien::Cartesien(const double x, const double y) : _x(x), _y(y) {}
+
+Cartesien::Cartesien(const Polaire& p) 
+    : _x(p.getDistance() * cos(p.getAngle()*3.14159/180)),
+      _y(p.getDistance() * sin(p.getAngle()*3.14159/180)) {}
+
+void Cartesien::afficher(std::ostream& stream) const {
+    stream << "(x=" << _x << ";y=" << _y << ")";
+}
+
+void Cartesien::convertir(Polaire& p) const {
+    p.setAngle(std::atan2(_y,_x)*180/3.14159);
+    p.setDistance(std::sqrt((_x*_x) + (_y*_y)));
+}
+
+void Cartesien::convertir(Cartesien& c) const {
+    c.setX(_x);
+    c.setY(_y);
+}
+
+void Cartesien::setX(const double x) {
+    _x = x;
+}
+
+void Cartesien::setY(const double y) {
+    _y = y;
+}
+
+double Cartesien::getX() const{
+    return _x;
+}
+
+double Cartesien::getY() const{
+    return _y;
+}
