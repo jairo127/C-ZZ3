aeb705ee2ae562b0539cdd208d73e26de12eeb27
William <william.garrier63@orange.fr>
2021-11-09 10:18:03 +0100

    Initial commit [TP5]
    Définir d'abord la classe Valeur, on stockera dans un premier temps un simple réel. Tests 1-4

---
+++ TP5/src/valeur.hpp
@@ -0,0 +1,20 @@
+#ifndef __CPP5_ZZ3_VALEUR__
+#define __CPP5_ZZ3_VALEUR__
+
+class Valeur {
+    private:
+        double _valeur;
+
+    public:
+        Valeur(double valeur = 0) : _valeur(valeur) {}
+
+        void setNombre(const double valeur) {
+            _valeur = valeur;
+        }
+
+        double getNombre() const {
+            return _valeur;
+        }
+};
+
+#endif
